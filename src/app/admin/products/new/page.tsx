933182619cd07099b3c67a75a4c231fb2c2c28eb
Robert (Ricky) Villareal <112342959+rrvillareal23@users.noreply.github.com>
2024-06-07 12:07:59 -0700

    added product from

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageHeader from "../../_components/PageHeader";
+import ProductForm from "../_componets/ProductForm";
+
+const NewPorductPage = () => {
+  return (
+    <>
+      <PageHeader>Add Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+};
+
+export default NewPorductPage;
