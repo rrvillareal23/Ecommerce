32de8292842568f33ec50c0d8c9785698012d1fa
Robert (Ricky) Villareal <112342959+rrvillareal23@users.noreply.github.com>
2024-06-08 12:53:02 -0700

    adding edit schema

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -3,7 +3,7 @@
 import db from "@/db/db"
 import {z} from "zod"
 import fs from "fs/promises"
-import { redirect } from "next/navigation"
+import { notFound, redirect } from "next/navigation"
 
 const fileSchema = z.instanceof(File, {message: "Required"})
 
@@ -17,7 +17,7 @@ const addSchema = z.object({
     image: imageSchema.refine(file => file.size > 0, "Required")
 })
 
-export async function addProduct(prevState:unknown, formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
     const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
     if(result.success === false) {
         return result.error.formErrors.fieldErrors
@@ -43,4 +43,59 @@ export async function addProduct(prevState:unknown, formData: FormData) {
     }})
 
     redirect("/admin/products")
+}
+
+const editSchema = addSchema.extend({
+    file: fileSchema.optional(),
+    image: imageSchema.optional()
+})
+export async function updateProduct(id: string, prevState: unknown, formData: FormData) {
+    const result = editSchema.safeParse(Object.fromEntries(formData.entries()))
+    if(result.success === false) {
+        return result.error.formErrors.fieldErrors
+    }
+
+    const data = result.data
+    const product = await db.product.findUnique({where: {id}})
+
+    if (product === null) return notFound()
+
+    let filePath = product.filePath
+    if (data.file != null && data.file.size > 0){
+        await fs.unlink(product.filePath)
+        filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+        await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+    }
+
+    let imagePath = product.imagePath
+    if (data.image != null && data.image.size > 0){
+        await fs.unlink(`public${product.imagePath}`)
+        imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+        await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+    }
+
+    await db.product.update({ where: {id}, data : {
+        isAvailableForPurchase: false,
+        name: data.name,
+        description: data.description,
+        priceInCents: data.priceInCents,
+        filePath,
+        imagePath
+    }})
+
+    redirect("/admin/products")
+}
+
+export async function toggleProductAvailability(id: string, isAvailableForPurchase: boolean) {
+    await db.product.update({where: {id}, data: {isAvailableForPurchase}})
+}
+
+export async function deleteProduct(id:string) {
+    const product = await db.product.delete({where: {id}})
+
+    if (product== null ) return notFound()
+    
+    await fs.unlink(product.filePath)
+    await fs.unlink(`public${product.imagePath}`)
+
 }
