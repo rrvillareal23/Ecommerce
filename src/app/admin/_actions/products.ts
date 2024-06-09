e4a964a7f887bfc850cf6fd4e5441c4a5a41d26c
Robert (Ricky) Villareal <112342959+rrvillareal23@users.noreply.github.com>
2024-06-09 07:31:22 -0700

    small change to update

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -75,7 +75,6 @@ export async function updateProduct(id: string, prevState: unknown, formData: Fo
     }
 
     await db.product.update({ where: {id}, data : {
-        isAvailableForPurchase: false,
         name: data.name,
         description: data.description,
         priceInCents: data.priceInCents,
